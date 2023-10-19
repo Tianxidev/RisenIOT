44ede018774a8e46d22f02ddd66a9dbb7f1efeb6
Tianxidev <2200475850@qq.com>
2023-10-19 10:32:31 +0800

    修改环境变量库相关问题

--- backend/internal/env/env.go
+++ backend/internal/env/env.go
@@ -1,17 +1,42 @@
 package env
 
 import (
-	"fmt"
 	"gopkg.in/ini.v1"
-	"os"
 )
 
 // GetEnv 获取环境变量
 func GetEnv(key string) (string, error) {
+
 	cfg, err := ini.Load(".env")
 	if err != nil {
-		fmt.Printf("Fail to read file: %v", err)
-		os.Exit(1)
+		return "", err
 	}
-	return cfg.Section("").Key(key).String(), nil
+	service, err := cfg.GetSection("")
+	if err != nil {
+		return "", err
+	}
+	startKey, err := service.GetKey(key)
+	if err != nil {
+		return "", err
+	}
+	return startKey.Value(), nil
+
+}
+
+// SetEnv 设置环境变量
+func SetEnv(key string, value string) error {
+	cfg, err := ini.Load(".env")
+	if err != nil {
+		return err
+	}
+	service, err := cfg.GetSection("")
+	if err != nil {
+		return err
+	}
+	targetKey := service.Key(key)
+	if err != nil {
+		return err
+	}
+	targetKey.SetValue(value)
+	return cfg.SaveTo(".env")
 }
