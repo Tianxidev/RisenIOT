4985a0f3b943fe356c093bde378a3f87ea7688fa
Tianxidev <2200475850@qq.com>
2023-10-21 01:23:15 +0800

    修复 casbin 配置在 linux 找不到的问题

--- backend/internal/casbin/casbin.go
+++ backend/internal/casbin/casbin.go
@@ -3,14 +3,13 @@ package casbin
 import (
 	"RisenIOT/backend/global"
 	"github.com/casbin/casbin/v2"
-	"os"
 )
 
 // SetupCasbinEnforcer 初始化casbin
 func SetupCasbinEnforcer() error {
-	dir, _ := os.Getwd()
-	modelPath := dir + "\\config\\rbac_model.conf"
-	csvPath := dir + "\\config\\rbac.csv"
+	//dir, _ := os.Getwd()
+	modelPath := "./config/rbac_model.conf"
+	csvPath := "./config/rbac.csv"
 	global.Logger.INFO("加载访问控制模型:" + modelPath)
 	global.Logger.INFO("加载路由控制表:" + csvPath)
 	var err error
